036470d9fc91470a531ffdb44b45f1182056b457
Gian Boschetti <gianboshetti@icloud.com>
2019-09-15 20:52:46 -0300

    prepare connection with .env
    o arquivo .env deve ser colocado na raiz do projeto, na mesma pasta do
    .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const pg = require('pg');
+const pool = new pg.Pool({
+  user:     process.env.user,
+  host:     process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port:     process.env.port,
+  scheme:   process.env.scheme
+});
+
+pool.query(`SELECT * FROM findev.\"User\"`, (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+
 app.get('/', (req, res) => {
   res.send('Running');
 });
+
 app.listen(port, err => {
   if (err) {
     return console.error(err);
