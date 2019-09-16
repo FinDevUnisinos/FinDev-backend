6772cdd8721c5ae58205f86d9b5f7c237c4608bb
Pedro Bohlmann Cascaes Silva <pedro.b.c.silva@gmail.com>
2019-09-16 01:21:16 -0300

    chore(build): adjusting project build

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-const dotenv = require('dotenv');
+import express from "express"
+import dotenv from 'dotenv'
 dotenv.config();
 
-const express = require("express");
+import { config } from './config'
+
 const app = express();
 const port = 3000;
 
 const pg = require('pg');
-const pool = new pg.Pool({
-  user:     process.env.user,
-  host:     process.env.host,
-  database: process.env.database,
-  password: process.env.password,
-  port:     process.env.port,
-  scheme:   process.env.scheme
-});
+const pool = new pg.Pool(config);
 
 pool.query(`SELECT * FROM findev.\"User\"`, (err, res) => {
   console.log(err, res);
