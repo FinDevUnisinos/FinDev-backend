af95da1bec9eaad03c7eddacd1bbfe033b30a35a
Gian Boschetti <gianboschetti@icloud.com>
2019-11-24 16:05:26 -0300

    insert, update and delete user skills

--- src/api/UserAPI.ts
+++ src/api/UserAPI.ts
@@ -18,13 +18,13 @@ const userController = new UserController
 const route = new Route
 
 userApp.post(route.getUserRoute() + '/login', async (req, res) => {
-	
+
 	const userController = new UserController
-	
+
 	asyncConnection().then(async connection => {
 		const email = req.body.email
 		const password = sessionController.hashPassword(req.body.password)
-	
+
 		if ((await userController.veryfyPassword(email, password)) !== undefined) {
 			const user = await userController.getUserByEmail(email)
 			const name = user.name
@@ -37,38 +37,38 @@ userApp.post(route.getUserRoute() + '/login', async (req, res) => {
 });
 
 userApp.get(route.getUserRoute() + '/all', authApp, async (req, res) => {
-	
+
 	const userController = new UserController
-	
+
 	asyncConnection().then(async () => {
 		res.send(await userController.getUsers())
 	})
 });
 
 userApp.post(route.getUserRoute() + '/getUser', authApp, async (req, res) => {
-	
+
 	const validToken = sessionController.validateToken(req.headers['x-access-token'].toString())
-	
+
 	asyncConnection().then(async () => {
 		const user = await userController.getUserByEmail(validToken.body.email.toString())
 		res.send(user)
 	})
 })
-  
+
 userApp.post(route.getUserRoute() + '/signup', async (req, res) => {
-	
+
 	const userController = new UserController
-	
+
 	try {
 		asyncConnection().then(async () => {
-			
+
 			//find an existing user
 			const user = await userController.getUserByEmail(req.body.email.toString());
-			
+
 			if (user != undefined) {
 				res.status(400).send("User already registered.");
 			} else {
-				
+
 				const createdUser = new User()
 				createdUser.NewUser(
 					req.body.name,
@@ -76,7 +76,7 @@ userApp.post(route.getUserRoute() + '/signup', async (req, res) => {
 					sessionController.hashPassword(req.body.password),
 					req.body.userType
 				)
-				
+
 				userController.addUser(createdUser)
 				res.send(sessionController.generateToken(createdUser.name, createdUser.email))
 
@@ -88,13 +88,13 @@ userApp.post(route.getUserRoute() + '/signup', async (req, res) => {
 });
 
 userApp.post(route.getUserSkillsRoute() + '/insert', authApp, async (req, res, next) => {
-	
+
 	const validToken = sessionController.validateToken(req.headers['x-access-token'].toString())
-	
+
 	asyncConnection().then(async () => {
-	
+
 		const user = await userController.getUserByEmail(validToken.body.email.toString())
-	
+
 		if (user.userType === UserTypes.EMPLOYEE) {
 
 			const skillId = Number.parseInt(req.body.skillId)
@@ -109,14 +109,42 @@ userApp.post(route.getUserSkillsRoute() + '/insert', authApp, async (req, res, n
 	})
 });
 
-userApp.post(route.getUserSkillsRoute() + '/all', authApp, async (req, res, next) => {
-	
+userApp.post(route.getUserSkillsRoute() + '/delete', authApp, async (req, res, next) => {
+
 	const validToken = sessionController.validateToken(req.headers['x-access-token'].toString())
-	
+
 	asyncConnection().then(async () => {
-	
+
+		const user = await userController.getUserByEmail(validToken.body.email.toString())
+
+		if (user.userType === UserTypes.EMPLOYEE) {
+
+			const skillId = Number.parseInt(req.body.skillId)
+			const skillIsMine = await userController.validateSkillIsMine(skillId, user)
+
+			if (skillIsMine == true) {
+
+				await userController.deleteSkillFromUser(user.id, skillId);
+				res.send("Skill successfully deleted from User")
+
+			} else {
+				res.status(403).send("This Skill not yours!")
+			}
+
+		} else {
+			res.status(403).send("You cannot delete a skill on an user since you aren't an EMPLOYEE")
+		}
+	})
+});
+
+userApp.post(route.getUserSkillsRoute() + '/all', authApp, async (req, res, next) => {
+
+	const validToken = sessionController.validateToken(req.headers['x-access-token'].toString())
+
+	asyncConnection().then(async () => {
+
 		const user = await userController.getUserByEmail(validToken.body.email.toString())
 		res.send(await userController.getUsersWithSkills(user))
-	
+
 	})
 });
